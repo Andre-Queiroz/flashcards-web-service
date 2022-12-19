ac1a62d353c971a167e0952ff8862fbf2258321e
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2022-12-18 21:05:45 -0300

    Service layer to create and find flashcards

--- src/main/java/com/queiroz/flashcards/service/flashcard/FlashcardService.java
+++ src/main/java/com/queiroz/flashcards/service/flashcard/FlashcardService.java
@@ -1,5 +1,12 @@
 package com.queiroz.flashcards.service.flashcard;
 
+import com.queiroz.flashcards.model.flashcard.Flashcard;
+
+import java.util.Optional;
+
 public interface FlashcardService {
 
+    Optional<Flashcard> create(final Flashcard flashcard);
+
+    Optional<Flashcard> find(final String id);
 }
--- src/main/java/com/queiroz/flashcards/service/flashcard/FlashcardServiceImpl.java
+++ src/main/java/com/queiroz/flashcards/service/flashcard/FlashcardServiceImpl.java
@@ -1,10 +1,25 @@
 package com.queiroz.flashcards.service.flashcard;
 
+import com.queiroz.flashcards.model.flashcard.Flashcard;
+import com.queiroz.flashcards.repository.flashcard.FlashcardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class FlashcardServiceImpl implements FlashcardService {
 
+    private final FlashcardRepository flashcardRepository;
+
+    @Override
+    public Optional<Flashcard> create(final Flashcard flashcard) {
+        return Optional.of(flashcardRepository.save(flashcard));
+    }
+
+    @Override
+    public Optional<Flashcard> find(final String id) {
+        return flashcardRepository.findById(id);
+    }
 }
