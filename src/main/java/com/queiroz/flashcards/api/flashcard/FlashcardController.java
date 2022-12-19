0c448582467fa98e02a40a111bebf79c0cda373a
Andre-Queiroz <andretorresdequeiroz@gmail.com>
2022-12-18 21:03:50 -0300

    Flashcard api with create and find endpoints

--- src/main/java/com/queiroz/flashcards/api/flashcard/FlashcardController.java
+++ src/main/java/com/queiroz/flashcards/api/flashcard/FlashcardController.java
@@ -1,7 +1,17 @@
 package com.queiroz.flashcards.api.flashcard;
 
+import com.queiroz.flashcards.api.flashcard.json.FlashcardRequest;
+import com.queiroz.flashcards.model.flashcard.Flashcard;
+import com.queiroz.flashcards.service.flashcard.FlashcardService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class FlashcardController {
 
+    private final FlashcardService flashcardService;
+
+    @PostMapping
+    public ResponseEntity<Flashcard> createFlashcard(@RequestBody @Valid final FlashcardRequest flashcardRequest) {
+        var response = flashcardService.create(FlashcardRequest.toFlashcard(flashcardRequest));
+        return response.map(flashcard -> new ResponseEntity<>(flashcard, HttpStatus.CREATED))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Flashcard> findFlashcard(@PathVariable("id") final String id) {
+        var response = flashcardService.find(id);
+        return response.map(flashcard -> new ResponseEntity<>(flashcard, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+    }
 
 }
